e938dc75941d59e92c65f8d7e8ddb0b5874fe371
Filipe Alves <filipemouraalves@gmail.com>
2021-04-20 17:05:10 -0300

    Melhorias na pesquisa de contatos

--- src/main/java/com/github/filipealves/agendaapi/model/api/rest/ContatoController.java
+++ src/main/java/com/github/filipealves/agendaapi/model/api/rest/ContatoController.java
@@ -1,16 +1,24 @@
 package com.github.filipealves.agendaapi.model.api.rest;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.Part;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +30,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/contatos")
+@CrossOrigin("*")
 public class ContatoController {
 	
 	private final ContatoRepository contatoRepository;
@@ -44,12 +53,32 @@ public class ContatoController {
 	}
 	
 	@PatchMapping("{id}/favorito")
-	public void favorite(@PathVariable Integer id, @RequestBody Boolean favorito) {
+	public void favorite(@PathVariable Integer id) {
 		Optional<Contato> contatoPesquisado = contatoRepository.findById(id);
 		contatoPesquisado.ifPresent(contato -> {
-			contato.setFavorito(favorito);
+			boolean favorito = contato.getFavorito() == Boolean.TRUE;
+			contato.setFavorito(!favorito);
 			contatoRepository.save(contato);
 		});
+	}
+	
+	@PutMapping("{id}/foto")
+	public byte[] addPhoto(@PathVariable Integer id, 
+						   @RequestParam("foto") Part arquivo) {
+		Optional<Contato> contato = contatoRepository.findById(id);
+		return contato.map(contact -> {
+			try {
+				InputStream is = arquivo.getInputStream();
+				byte[] bytes = new byte[(int) arquivo.getSize()];
+				IOUtils.readFully(is, bytes);
+				contact.setFoto(bytes);
+				contatoRepository.save(contact);		
+				is.close();
+				return bytes;
+				} catch (IOException e) {
+					return null;
+			}
+		}).orElse(null);
 	}
 
 }
